12f1780e0a3f36680ead1eae6ad54298dd179bdf
baeseohyeon <formnarago@naver.com>
2021-10-01 20:08:14 +0900

    ci:init

--- src/test/java/com/highgeupsik/backend/BackendApplicationTests.java
+++ src/test/java/com/highgeupsik/backend/BackendApplicationTests.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@SpringBootTest
-@EnableJpaAuditing
+@SpringBootTest
 class BackendApplicationTests {
 
 	@Test
