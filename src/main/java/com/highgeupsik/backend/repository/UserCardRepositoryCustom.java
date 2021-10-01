edab8b5bdd9f20a93b1f68bc434fabf839d6eca5
baeseohyeon <formnarago@naver.com>
2021-10-01 19:07:10 +0900

    test2

--- src/main/java/com/highgeupsik/backend/repository/UserCardRepositoryCustom.java
+++ src/main/java/com/highgeupsik/backend/repository/UserCardRepositoryCustom.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface UserCardRepositoryCustom {
+    
 
     Page<UserCardResDTO> findUserCards(Pageable pageable);
 
