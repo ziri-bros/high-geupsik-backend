ce7476f8882df23e08d52187f3fd84e6ddb50d9f
baeseohyeon <formnarago@naver.com>
2021-10-05 23:32:39 +0900

    chore: Follow Entity 와 localFile 저장 기능 삭제

--- src/main/java/com/highgeupsik/backend/BackendApplication.java
+++ src/main/java/com/highgeupsik/backend/BackendApplication.java
@@ -1,8 +1,6 @@
 package com.highgeupsik.backend;
-import com.highgeupsik.backend.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
