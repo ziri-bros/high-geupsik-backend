35bcc3f2a7b541e9e79a5c4a22f65b8bfbb942f4
baeseohyeon <formnarago@naver.com>
2021-11-16 18:23:56 +0900

    refactor: BoardDetailApiController를 BoardApiController로 변경

--- src/main/java/com/highgeupsik/backend/api/BoardApiController.java
+++ src/main/java/com/highgeupsik/backend/api/BoardApiController.java
@@ -1,15 +1,15 @@
 package com.highgeupsik.backend.api;
 
+import com.highgeupsik.backend.dto.BoardResDTO;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import com.highgeupsik.backend.dto.BoardDetailReqDTO;
-import com.highgeupsik.backend.dto.BoardDetailResDTO;
+import com.highgeupsik.backend.dto.BoardReqDTO;
 import com.highgeupsik.backend.dto.BoardSearchCondition;
 import com.highgeupsik.backend.exception.NotMatchException;
 import com.highgeupsik.backend.resolver.LoginUser;
-import com.highgeupsik.backend.service.BoardDetailQueryService;
-import com.highgeupsik.backend.service.BoardDetailService;
+import com.highgeupsik.backend.service.BoardQueryService;
+import com.highgeupsik.backend.service.BoardService;
 import com.highgeupsik.backend.service.LikeService;
 import com.highgeupsik.backend.utils.ApiResult;
 import com.highgeupsik.backend.utils.ApiUtils;
@@ -25,65 +25,65 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class BoardDetailApiController {
+public class BoardApiController {
 
-    private final BoardDetailQueryService boardDetailQueryService;
-    private final BoardDetailService boardDetailService;
+    private final BoardQueryService boardQueryService;
+    private final BoardService boardService;
     private final LikeService likeService;
 
     @GetMapping("/boards/{boardId}") //게시글 조회
-    public ApiResult<BoardDetailResDTO> board(@PathVariable("boardId") Long boardId) {
-        return ApiUtils.success(boardDetailQueryService.findOneById(boardId));
+    public ApiResult<BoardResDTO> board(@PathVariable("boardId") Long boardId) {
+        return ApiUtils.success(boardQueryService.findOneById(boardId));
     }
 
     @GetMapping("/boards") //게시글목록 조회
-    public ApiResult<Page<BoardDetailResDTO>> boards(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
+    public ApiResult<Page<BoardResDTO>> boards(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
         @LoginUser Long userId, BoardSearchCondition condition) {
-        return ApiUtils.success(boardDetailQueryService.findAll(userId, pageNum, condition));
+        return ApiUtils.success(boardQueryService.findAll(userId, pageNum, condition));
     }
 
     @PostMapping("/boards") //게시글 작성
     public ApiResult writeBoard(@LoginUser Long userId,
-        @RequestBody BoardDetailReqDTO boardDetailReqDTO) {
-        if (!boardDetailReqDTO.getUploadFileDTOList().isEmpty()) {
-            return ApiUtils.success(boardDetailService.saveBoard(userId, boardDetailReqDTO));
+        @RequestBody BoardReqDTO boardReqDTO) {
+        if (!boardReqDTO.getUploadFileDTOList().isEmpty()) {
+            return ApiUtils.success(boardService.saveBoard(userId, boardReqDTO));
         }
-        return ApiUtils.success(boardDetailService.saveBoard(userId, boardDetailReqDTO.getTitle(),
-            boardDetailReqDTO.getContent(), boardDetailReqDTO.getCategory()));
+        return ApiUtils.success(boardService.saveBoard(userId, boardReqDTO.getTitle(),
+            boardReqDTO.getContent(), boardReqDTO.getCategory()));
     }
 
     @GetMapping("/boards/{boardId}/edit") //게시글 편집창
-    public ApiResult<BoardDetailResDTO> editBoard(@PathVariable("boardId") Long boardId,
+    public ApiResult<BoardResDTO> editBoard(@PathVariable("boardId") Long boardId,
         @LoginUser Long userId) {
-        BoardDetailResDTO boardDetailResDTO = boardDetailQueryService.findOneById(boardId);
-        Long writerId = boardDetailResDTO.getWriterId();
+        BoardResDTO boardResDTO = boardQueryService.findOneById(boardId);
+        Long writerId = boardResDTO.getWriterId();
         if (writerId.equals(userId)) {
-            return ApiUtils.success(boardDetailResDTO);
+            return ApiUtils.success(boardResDTO);
         }
         throw new NotMatchException(ErrorMessage.WRITER_NOT_MATCH);
     }
 
     @PutMapping("/boards/{boardId}") //게시글 편집
     public ApiResult editBoard(@PathVariable("boardId") Long boardId,
-        @LoginUser Long userId, @RequestBody BoardDetailReqDTO boardDetailReqDTO) {
-        Long writerId = boardDetailQueryService.findWriterIdByBoardId(boardId);
+        @LoginUser Long userId, @RequestBody BoardReqDTO boardReqDTO) {
+        Long writerId = boardQueryService.findWriterIdByBoardId(boardId);
         if (writerId.equals(userId)) {
-            return ApiUtils.success(boardDetailService.updateBoard(boardId, boardDetailReqDTO));
+            return ApiUtils.success(boardService.updateBoard(boardId, boardReqDTO));
         }
         throw new NotMatchException(ErrorMessage.WRITER_NOT_MATCH);
     }
 
     @DeleteMapping("/boards/{boardId}") //게시글 삭제
     public ApiResult deleteBoard(@PathVariable("boardId") Long boardId, @LoginUser Long userId) {
-        boardDetailService.deleteBoard(userId, boardId);
+        boardService.deleteBoard(userId, boardId);
         return ApiUtils.success(null);
     }
 
     @GetMapping("/boards/my") //내가 작성한 게시글
-    public ApiResult<List<BoardDetailResDTO>> myBoards(
+    public ApiResult<List<BoardResDTO>> myBoards(
         @RequestParam(value = "page", defaultValue = "1") Integer pageNum,
         @LoginUser Long userId) {
-        return ApiUtils.success(boardDetailQueryService.findByMyId(userId, pageNum));
+        return ApiUtils.success(boardQueryService.findByMyId(userId, pageNum));
     }
 
     @PostMapping("/boards/{boardId}/like") //게시글 좋아요
