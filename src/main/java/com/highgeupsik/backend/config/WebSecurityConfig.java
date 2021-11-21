fc8719d8faea3d82dc3b721ca0a33915e891ad26
baeseohyeon <formnarago@naver.com>
2021-11-21 20:46:47 +0900

    feat: webignoring 추가

--- src/main/java/com/highgeupsik/backend/config/WebSecurityConfig.java
+++ src/main/java/com/highgeupsik/backend/config/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,6 +38,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/favicon.ico",
+            "/v2/**",
+            "/webjars/**",
+            "/swagger**",
+            "/swagger-resources/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
