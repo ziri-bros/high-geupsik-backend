0494eb8f3e2b5b70a45b10d00ccca5c4742c6446
baeseohyeon <formnarago@naver.com>
2021-11-26 03:52:06 +0900

    refactor: School추가 uploadeFile을 String으로 변경

--- src/main/java/com/highgeupsik/backend/service/UserCardService.java
+++ src/main/java/com/highgeupsik/backend/service/UserCardService.java
@@ -1,16 +1,16 @@
 package com.highgeupsik.backend.service;
 
 
+import static com.highgeupsik.backend.utils.ErrorMessage.SCHOOL_NOT_FOUND;
 import static com.highgeupsik.backend.utils.ErrorMessage.USER_NOT_FOUND;
 
-import com.highgeupsik.backend.dto.UploadFileDTO;
-import com.highgeupsik.backend.dto.UserCardReqDTO;
-import com.highgeupsik.backend.entity.UploadFile;
+import com.highgeupsik.backend.dto.SchoolDTO;
+
 import com.highgeupsik.backend.entity.UserCard;
 import com.highgeupsik.backend.exception.NotFoundException;
+import com.highgeupsik.backend.repository.SchoolRepository;
 import com.highgeupsik.backend.repository.UserCardRepository;
 import com.highgeupsik.backend.repository.UserRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,12 +22,15 @@ public class UserCardService {
 
     private final UserCardRepository userCardRepository;
     private final UserRepository userRepository;
+    private final SchoolRepository schoolRepository;
 
-    public Long saveUserCard(Long userId, UploadFileDTO uploadFileDTO) {
+    public Long saveUserCard(Long userId, String thumbnail, SchoolDTO schoolDTO) {
         return userCardRepository.save(UserCard.builder()
             .user(userRepository.findById(userId).orElseThrow(() ->
                 new NotFoundException(USER_NOT_FOUND)))
-            .uploadFile(new UploadFile(uploadFileDTO.getFileName(), uploadFileDTO.getFileDownloadUri()))
+            .thumbnail(thumbnail)
+            .school(schoolRepository.findByName(schoolDTO.getName()).orElseThrow(() ->
+                new NotFoundException(SCHOOL_NOT_FOUND)))
             .build()).getId();
     }
 
