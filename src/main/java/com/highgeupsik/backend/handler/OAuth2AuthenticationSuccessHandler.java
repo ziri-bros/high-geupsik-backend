85f38166be2c4d6d54e494877c0cd32f21c33cac
baeseohyeon <formnarago@naver.com>
2021-11-25 22:02:50 +0900

    refactor: 유저정보와 토큰을queryParameter로 담아서 redirect해주는 로직으로 변경

--- src/main/java/com/highgeupsik/backend/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/highgeupsik/backend/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,11 +1,10 @@
 package com.highgeupsik.backend.handler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.highgeupsik.backend.dto.TokenDTO;
 import com.highgeupsik.backend.entity.User;
+import com.highgeupsik.backend.exception.NotFoundException;
 import com.highgeupsik.backend.jwt.JwtTokenProvider;
 import com.highgeupsik.backend.oauth2.SocialUser;
-import com.highgeupsik.backend.utils.ApiUtils;
+import com.highgeupsik.backend.service.UserCardQueryService;
 import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,24 +13,47 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final ObjectMapper objectMapper;
     private final JwtTokenProvider jwtTokenProvider;
+    private final UserCardQueryService userCardQueryService;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException {
         log.info(authentication.getAuthorities().toString());
+        String targetUrl = determineTargetUrl(request, response, authentication);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) {
+        String targetUrl = "http://10.21.20.153:3000/oauth2/redirect?";
         SocialUser socialUser = (SocialUser) authentication.getPrincipal();
         User user = socialUser.getUser();
         String accessToken = jwtTokenProvider.createNewToken(user.getId(), user.getRole().toString(), "access");
         String refreshToken = jwtTokenProvider.createRefreshToken();
-        response.getWriter()
-            .write(this.objectMapper.writeValueAsString((ApiUtils.success(new TokenDTO(accessToken, refreshToken)))));
+        boolean isSubmittedCard = checkUserCard(user.getId());
+        return UriComponentsBuilder.fromUriString(targetUrl)
+            .queryParam("role", user.getRole())
+            .queryParam("isSubmittedCard", isSubmittedCard)
+            .queryParam("accessToken", accessToken)
+            .queryParam("refreshToken", refreshToken)
+            .build()
+            .toUriString();
+    }
+
+    public boolean checkUserCard(Long userId) {
+        try {
+            userCardQueryService.findByUserId(userId);
+            return true;
+        } catch (NotFoundException e) {
+            return false;
+        }
     }
 }
