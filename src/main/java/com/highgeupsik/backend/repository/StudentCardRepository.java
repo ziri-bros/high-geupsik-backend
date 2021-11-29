861f751ae45c3655a40a50f4f1388c1122140e01
baeseohyeon <formnarago@naver.com>
2021-11-29 21:41:30 +0900

    feat: StudentCard 엔티티, 레파지토리, 서비스 추가

---
+++ src/main/java/com/highgeupsik/backend/repository/StudentCardRepository.java
@@ -0,0 +1,12 @@
+package com.highgeupsik.backend.repository;
+
+import com.highgeupsik.backend.entity.StudentCard;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface StudentCardRepository extends JpaRepository<StudentCard, Long> {
+
+    Optional<StudentCard> findByUserId(Long userId);
+
+}
