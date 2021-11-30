2e77aeb35c8009838692d5e26bd6b5f372ffd68b
audgnssweet <audgnssweet@naver.com>
2021-12-01 00:55:06 +0900

    feat(CommentRepository): findById를 Overriding
    - fetch join으로 한번에 board, parent comment를 가져오기 위해

--- src/main/java/com/highgeupsik/backend/repository/CommentRepository.java
+++ src/main/java/com/highgeupsik/backend/repository/CommentRepository.java
@@ -1,23 +1,27 @@
 package com.highgeupsik.backend.repository;
 
-import com.highgeupsik.backend.entity.Board;
-import com.highgeupsik.backend.entity.Comment;
-import com.highgeupsik.backend.entity.User;
-
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.highgeupsik.backend.entity.Board;
+import com.highgeupsik.backend.entity.Comment;
+import com.highgeupsik.backend.entity.User;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
-    Page<Comment> findByUserId(Long userId, Pageable pageable);
+	@EntityGraph(attributePaths = {"board", "parent"})
+	Optional<Comment> findById(Long commentId);
 
-    Page<Comment> findByBoardIdAndParent(Long postId, Comment parent, Pageable pageable);
+	Page<Comment> findByUserId(Long userId, Pageable pageable);
 
-    List<Comment> findByUserIdAndBoardId(Long userId, Long postId);
+	Page<Comment> findByBoardIdAndParent(Long postId, Comment parent, Pageable pageable);
 
-    Optional<Comment> findFirstByBoardAndUser(Board board, User user);
+	List<Comment> findByUserIdAndBoardId(Long userId, Long postId);
+
+	Optional<Comment> findFirstByBoardAndUser(Board board, User user);
 }
