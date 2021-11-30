3abe3020cc103d4d8760afce32627825d9a86d27
baeseohyeon <formnarago@naver.com>
2021-12-01 00:02:12 +0900

    chore: check utils 삭제

--- src/main/java/com/highgeupsik/backend/utils/checkUtils.java
+++
@@ -1,9 +0,0 @@
-package com.highgeupsik.backend.utils;
-
-public class checkUtils {
-
-    public static boolean isSameId(Long userId, Long targetId){
-        return userId.equals(targetId);
-    }
-
-}
