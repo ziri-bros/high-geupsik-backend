94c530139df8ae24776bea59627bf4147be90309
baeseohyeon <formnarago@naver.com>
2021-12-01 00:01:56 +0900

    refactor: student-card-entity 학년 반 컬럼 추가

--- src/main/java/com/highgeupsik/backend/dto/UserResDTO.java
+++ src/main/java/com/highgeupsik/backend/dto/UserResDTO.java
@@ -1,7 +1,6 @@
 package com.highgeupsik.backend.dto;
 
 
-import com.highgeupsik.backend.entity.Role;
 import com.highgeupsik.backend.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +12,8 @@ public class UserResDTO {
     private Long id;
     private String email;
     private String username;
+    private int grade;
+    private int classNum;
     private String studentCardImage;
     private SchoolDTO schoolDTO;
 
@@ -20,6 +21,8 @@ public class UserResDTO {
         id = user.getId();
         email = user.getEmail();
         username = user.getUsername();
+        grade = user.getStudentCard().getGrade().getGradeNum();
+        classNum = user.getStudentCard().getClassNum();
         studentCardImage = user.getStudentCard().getStudentCardImage();
         schoolDTO = new SchoolDTO(user.getSchool());
     }
--- src/main/java/com/highgeupsik/backend/service/UserService.java
+++ src/main/java/com/highgeupsik/backend/service/UserService.java
@@ -6,7 +6,9 @@ import static com.highgeupsik.backend.utils.ErrorMessage.TOKEN_EXPIRED;
 import static com.highgeupsik.backend.utils.ErrorMessage.USER_NOT_FOUND;
 
 import com.highgeupsik.backend.dto.SchoolDTO;
+import com.highgeupsik.backend.dto.StudentCardDTO;
 import com.highgeupsik.backend.dto.TokenDTO;
+import com.highgeupsik.backend.entity.GRADE;
 import com.highgeupsik.backend.entity.StudentCard;
 import com.highgeupsik.backend.entity.User;
 import com.highgeupsik.backend.exception.NotFoundException;
@@ -30,15 +32,16 @@ public class UserService {
     private final StudentCardRepository studentCardRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
-    public void updateUserInfo(Long userId, String studentCardImage, SchoolDTO schoolDTO) {
+    public void updateUserInfo(Long userId, StudentCardDTO studentCardDTO, SchoolDTO schoolDTO) {
         User user = userRepository.findById(userId).orElseThrow(
             () -> new NotFoundException(USER_NOT_FOUND));
         user.updateSchool(schoolRepository.findByName(schoolDTO.getName()).orElseThrow(
             () -> new NotFoundException(SCHOOL_NOT_FOUND)));
-        user.updateStudentCard(studentCardRepository.save(new StudentCard(studentCardImage)));
+        user.updateStudentCard(studentCardRepository.save(new StudentCard(
+            GRADE.from(studentCardDTO.getGrade()), studentCardDTO.getClassNum(), studentCardDTO.getStudentCardImage())));
     }
 
-    public void updateSchool(Long userId, SchoolDTO schoolDTO){
+    public void updateSchool(Long userId, SchoolDTO schoolDTO) {
         User user = userRepository.findById(userId).orElseThrow(
             () -> new NotFoundException(USER_NOT_FOUND));
         user.updateSchool(schoolRepository.findByName(schoolDTO.getName()).orElseThrow(
