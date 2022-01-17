45ec0f5c24650ba1db5322c89e856f5a39aef4d0
baeseohyeon <formnarago@naver.com>
2022-01-17 15:47:39 +0900

    chore: deleteByBoardId 삭제

--- src/main/java/com/highgeupsik/backend/repository/UploadFileRepository.java
+++ src/main/java/com/highgeupsik/backend/repository/UploadFileRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UploadFileRepository extends JpaRepository<UploadFile, Long> {
 
-    void deleteByBoardId(Long boardId);
 }
