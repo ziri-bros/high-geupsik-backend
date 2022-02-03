0e86185bac704eac550051ae5ba7eaeefc906b45
audgnssweet <audgnssweet@naver.com>
2022-02-04 01:40:06 +0900

    refactor: 이미지 업로드시 IO Exception을 서비스단에서 컨트롤하도록 변경

--- src/main/java/com/highgeupsik/backend/api/ImageApiController.java
+++ src/main/java/com/highgeupsik/backend/api/ImageApiController.java
@@ -1,26 +1,28 @@
 package com.highgeupsik.backend.api;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.highgeupsik.backend.dto.UploadFileDTO;
 import com.highgeupsik.backend.service.S3Service;
 import com.highgeupsik.backend.utils.ApiResult;
 import com.highgeupsik.backend.utils.ApiUtils;
+
 import io.swagger.annotations.ApiOperation;
-import java.io.IOException;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
 public class ImageApiController {
 
-    private final S3Service s3Service;
+	private final S3Service s3Service;
 
-    @ApiOperation(value = "이미지 업로드")
-    @PostMapping("/images")
-    public ApiResult<List<UploadFileDTO>> imageSave(List<MultipartFile> imageList) throws IOException {
-        return ApiUtils.success(s3Service.uploadFiles(imageList));
-    }
+	@ApiOperation(value = "이미지 업로드")
+	@PostMapping("/images")
+	public ApiResult<List<UploadFileDTO>> imageSave(List<MultipartFile> imageList) {
+		return ApiUtils.success(s3Service.uploadFiles(imageList));
+	}
 }
