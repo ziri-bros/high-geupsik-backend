3671a1408c0ccbe0e7b3380abd673cf0fbc716e4
baeseohyeon <formnarago@naver.com>
2022-02-06 19:07:07 +0900

    refactor: static 파일을 읽도록 변경

--- src/main/java/com/highgeupsik/backend/utils/ParsingUtils.java
+++ src/main/java/com/highgeupsik/backend/utils/ParsingUtils.java
@@ -2,9 +2,9 @@ package com.highgeupsik.backend.utils;
 
 import com.highgeupsik.backend.entity.Region;
 import com.highgeupsik.backend.entity.School;
-import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -12,11 +12,12 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.core.io.ClassPathResource;
 
 @Slf4j
 public class ParsingUtils {
 
-    public static List<School> getSchoolListFromJsonFile(String fileLocation) {
+    public static List<School> getSchoolListFromJsonFile(ClassPathResource resource) {
         String REGION = "LCTN_SC_NM"; //소재지명
         String SCHOOLCODE = "SD_SCHUL_CODE"; //표준학교코드
         String SCHOOLNAME = "SCHUL_NM"; //학교명
@@ -25,7 +26,8 @@ public class ParsingUtils {
         List<School> schoolList = new ArrayList<>();
         try {
             JSONParser parser = new JSONParser();
-            Reader reader = new FileReader(fileLocation);
+            InputStreamReader streamReader = new InputStreamReader(resource.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
             JSONArray jsonArray = (JSONArray) parser.parse(reader);
             for (int i = 1; i < jsonArray.size(); i++) {
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
