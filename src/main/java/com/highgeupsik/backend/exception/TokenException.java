2e693012528f2b999a5bf9eb9a61228095d998c5
baeseohyeon <formnarago@naver.com>
2022-02-08 16:14:19 +0900

    chore: 에러 이름 변경

---
+++ src/main/java/com/highgeupsik/backend/exception/TokenException.java
@@ -0,0 +1,8 @@
+package com.highgeupsik.backend.exception;
+
+public class TokenException extends RuntimeException {
+
+    public TokenException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/highgeupsik/backend/exception/TokenExpiredException.java
+++
@@ -1,8 +0,0 @@
-package com.highgeupsik.backend.exception;
-
-public class TokenExpiredException extends RuntimeException {
-
-    public TokenExpiredException(String message) {
-        super(message);
-    }
-}
