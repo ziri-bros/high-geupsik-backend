f9ec5da6ea5ce956385156ff427733ec3142462f
baeseohyeon <formnarago@naver.com>
2022-02-08 16:14:59 +0900

    chore: 스태틱 임포트 추가

--- src/main/java/com/highgeupsik/backend/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/highgeupsik/backend/handler/JwtAuthenticationEntryPoint.java
@@ -2,6 +2,8 @@ package com.highgeupsik.backend.handler;
 
 import static com.highgeupsik.backend.utils.ApiUtils.error;
 import static com.highgeupsik.backend.utils.ErrorMessage.NOT_USER;
+import static javax.servlet.http.HttpServletResponse.*;
+import static org.springframework.http.HttpStatus.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.highgeupsik.backend.utils.ApiError;
@@ -9,11 +11,9 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
 
 @RequiredArgsConstructor
 @Component
@@ -24,9 +24,9 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint { /
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
         AuthenticationException authException) throws IOException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=utf-8");
         response.getWriter().write(this.objectMapper.writeValueAsString(
-            (error(new ApiError(NOT_USER, HttpStatus.UNAUTHORIZED)))));
+            (error(new ApiError(NOT_USER, UNAUTHORIZED)))));
     }
 }
