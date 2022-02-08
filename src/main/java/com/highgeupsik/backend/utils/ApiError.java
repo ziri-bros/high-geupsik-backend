68e9f2826cc815f07bacd18f33fcefa9c64f9ef5
baeseohyeon <formnarago@naver.com>
2022-02-08 13:34:13 +0900

    chore: 에러 로그 띄우도록 변경

--- src/main/java/com/highgeupsik/backend/utils/ApiError.java
+++ src/main/java/com/highgeupsik/backend/utils/ApiError.java
@@ -1,15 +1,18 @@
 package com.highgeupsik.backend.utils;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@Slf4j
 public class ApiError {
 
     String message;
     HttpStatus httpStatus;
 
     public ApiError(String message, HttpStatus httpStatus) {
+        log.error(message);
         this.message = message;
         this.httpStatus = httpStatus;
     }
