061ed6f12d6381a9c43a2dc5b1de2b0baa4b7287
baeseohyeon <formnarago@naver.com>
2022-03-02 21:39:31 +0900

    feat: 관리자 로그인 DTO

---
+++ src/main/java/com/highgeupsik/backend/dto/LoginForm.java
@@ -0,0 +1,15 @@
+package com.highgeupsik.backend.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginForm {
+
+    private String loginId;
+
+    private String password;
+}
