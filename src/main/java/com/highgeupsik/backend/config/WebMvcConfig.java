8ba792846d5c526106b235c56c3c19873b0a2b30
baeseohyeon <formnarago@naver.com>
2022-03-02 21:38:55 +0900

    feat: 인터셉터 설정 추가

--- src/main/java/com/highgeupsik/backend/config/WebMvcConfig.java
+++ src/main/java/com/highgeupsik/backend/config/WebMvcConfig.java
@@ -1,10 +1,13 @@
 package com.highgeupsik.backend.config;
 
+import com.highgeupsik.backend.interceptor.LogInterceptor;
+import com.highgeupsik.backend.interceptor.LoginCheckInterceptor;
 import com.highgeupsik.backend.resolver.LoginUserArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -21,5 +24,18 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+            .order(1)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+            .order(2)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/", "/admin/login", "/admin/logout", "/css/**", "/*.ico", "/error");
     }
 }
