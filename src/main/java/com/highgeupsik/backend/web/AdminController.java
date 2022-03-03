35d54ed632cc7c2030d4f05f3afde70d5d523496
baeseohyeon <formnarago@naver.com>
2022-03-03 20:23:29 +0900

    feat: 관리자 컨트롤러 메소드 추가

--- src/main/java/com/highgeupsik/backend/web/AdminController.java
+++ src/main/java/com/highgeupsik/backend/web/AdminController.java
@@ -2,15 +2,17 @@ package com.highgeupsik.backend.web;
 
 import com.highgeupsik.backend.dto.UserConfirmDTO;
 import com.highgeupsik.backend.service.UserConfirmService;
+import com.highgeupsik.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -18,14 +20,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AdminController {
 
+    private final UserService userService;
     private final UserConfirmService userConfirmService;
 
-    @ResponseBody
     @GetMapping("/users")
     public String userConfirms(Model model,
         @RequestParam(value = "page", defaultValue = "1") Integer pageNum) {
         Page<UserConfirmDTO> userConfirms = userConfirmService.findAll(pageNum);
-        model.addAttribute("users", userConfirms.getContent());
-        return "OK"; //테스트
+        model.addAttribute("userConfirms", userConfirms.getContent());
+        return "home"; //테스트
+    }
+
+    @PostMapping("/users/{userId}/authorize") //수락
+    public String userAccept(@PathVariable Long userId) {
+        userService.acceptUser(userId);
+        return "redirect:/admin/users";
+    }
+
+    @PostMapping("/users/{userId}") //거부
+    public String userReject(@PathVariable Long userId) {
+        userService.rejectUser(userId);
+        return "redirect:/admin/users";
     }
 }
