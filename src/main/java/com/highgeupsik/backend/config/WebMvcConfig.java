2a49b1d85903596fe80286c4568e664fb22759fd
baeseohyeon <formnarago@naver.com>
2022-03-03 20:21:42 +0900

    chore: 인터셉터 삭제

--- src/main/java/com/highgeupsik/backend/config/WebMvcConfig.java
+++ src/main/java/com/highgeupsik/backend/config/WebMvcConfig.java
@@ -1,13 +1,10 @@
 package com.highgeupsik.backend.config;
 
-import com.highgeupsik.backend.interceptor.LogInterceptor;
-import com.highgeupsik.backend.interceptor.LoginCheckInterceptor;
 import com.highgeupsik.backend.resolver.LoginUserArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -24,18 +21,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginUserArgumentResolver());
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LogInterceptor())
-            .order(1)
-            .addPathPatterns("/**")
-            .excludePathPatterns("/css/**", "/*.ico", "/error");
-
-        registry.addInterceptor(new LoginCheckInterceptor())
-            .order(2)
-            .addPathPatterns("/**")
-            .excludePathPatterns("/", "/admin/login", "/admin/logout", "/css/**", "/*.ico", "/error");
     }
 }
