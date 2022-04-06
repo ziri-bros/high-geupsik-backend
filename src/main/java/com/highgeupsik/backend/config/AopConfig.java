ebf68bcf9642fbde0ba0cbe66f4cb4135cd14e75
baeseohyeon <formnarago@naver.com>
2022-04-06 20:41:48 +0900

    feat: aop 설정파일 추가

---
+++ src/main/java/com/highgeupsik/backend/config/AopConfig.java
@@ -0,0 +1,20 @@
+package com.highgeupsik.backend.config;
+
+import com.highgeupsik.backend.aop.aspect.RetryAspect;
+import com.highgeupsik.backend.aop.aspect.TraceAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AopConfig {
+
+    @Bean
+    public TraceAspect traceAspect() {
+        return new TraceAspect();
+    }
+
+    @Bean
+    public RetryAspect retryAspect() {
+        return new RetryAspect();
+    }
+}
