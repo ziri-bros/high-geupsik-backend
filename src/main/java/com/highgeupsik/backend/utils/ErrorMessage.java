88d965f0c09e0124b0dbffa1a29c3aab03fcec02
baeseohyeon <formnarago@naver.com>
2022-04-09 15:48:34 +0900

    chore: region, category null 메시지 추가

--- src/main/java/com/highgeupsik/backend/utils/ErrorMessage.java
+++ src/main/java/com/highgeupsik/backend/utils/ErrorMessage.java
@@ -8,10 +8,12 @@ public class ErrorMessage {
     public static final String WRITER_NOT_MATCH = "작성자가 아닙니다.";
     public static final String NOT_USER = "사용자가 아닙니다.";
     public static final String NOT_ROLE_USER = "권한이 없는 사용자 입니다.";
-    public static final String SCHOOL_NOT_FOUND = "없는 학교입니다";
-    public static final String GRADE_NOT_FOUND = "잘못된 학년 입니다";
+    public static final String SCHOOL_NOT_FOUND = "없는 학교입니다.";
+    public static final String GRADE_NOT_FOUND = "잘못된 학년 입니다.";
     public static final String TOKEN_EXPIRED = "만료된 토큰입니다.";
-    public static final String UNABLE_JWT_SIGNATURE = "잘못된 JWT 서명입니다";
-    public static final String UNSUPPORTED_JWT_TOKEN = "지원하지 않는 JWT 토큰입니다";
-    public static final String WRONG_JWT_TOKEN = "잘못된 JWT 토큰입니다";
+    public static final String UNABLE_JWT_SIGNATURE = "잘못된 JWT 서명입니다.";
+    public static final String UNSUPPORTED_JWT_TOKEN = "지원하지 않는 JWT 토큰입니다.";
+    public static final String WRONG_JWT_TOKEN = "잘못된 JWT 토큰입니다.";
+    public static final String REGION_NOT_NULL = "지역 입력은 필수입니다.";
+    public static final String CATEGORY_NOT_NULL = "카테고리 입력은 필수입니다.";
 }
