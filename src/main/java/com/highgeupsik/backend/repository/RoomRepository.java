4af1476586036b6cd9fdd1eb5c96d3d0089b7cb4
baeseohyeon <formnarago@naver.com>
2022-06-01 17:14:50 +0900

    chore: 방목록 조회 함수명 변경

--- src/main/java/com/highgeupsik/backend/repository/RoomRepository.java
+++ src/main/java/com/highgeupsik/backend/repository/RoomRepository.java
@@ -1,5 +1,6 @@
 package com.highgeupsik.backend.repository;
 
+import com.highgeupsik.backend.entity.Board;
 import com.highgeupsik.backend.entity.Room;
 import com.highgeupsik.backend.entity.User;
 import java.util.Optional;
@@ -7,9 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long>{
 
-    Optional<Room> findByFromUserAndToUser(User fromUser, User toUser);
+    Optional<Room> findByBoardAndFromUser(Board board, User fromUser);
 
-    Page<Room> findAllByFromUser(User fromUser, Pageable pageable);
+    Page<Room> findByFromUserId(Long fromUserId, Pageable pageable);
 }
