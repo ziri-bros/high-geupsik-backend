e4c751ce930f670433466334e40fe1471f9ca314
baeseohyeon <formnarago@naver.com>
2022-06-01 17:08:17 +0900

    chore: 대화방 사용자가 아닌 예러 메세지 추가

--- src/main/java/com/highgeupsik/backend/utils/ErrorMessage.java
+++ src/main/java/com/highgeupsik/backend/utils/ErrorMessage.java
@@ -5,11 +5,13 @@ public class ErrorMessage {
     public static final String USER_NOT_FOUND = "없는 사용자 입니다.";
     public static final String BOARD_NOT_FOUND = "없는 게시글 입니다.";
     public static final String COMMENT_NOT_FOUND = "없는 댓글 입니다.";
+    public static final String SCHOOL_NOT_FOUND = "없는 학교입니다.";
+    public static final String GRADE_NOT_FOUND = "잘못된 학년 입니다.";
+    public static final String ROOM_NOT_FOUND = "없는 대화방 입니다.";
     public static final String WRITER_NOT_MATCH = "작성자가 아닙니다.";
     public static final String NOT_USER = "사용자가 아닙니다.";
     public static final String NOT_ROLE_USER = "권한이 없는 사용자 입니다.";
-    public static final String SCHOOL_NOT_FOUND = "없는 학교입니다.";
-    public static final String GRADE_NOT_FOUND = "잘못된 학년 입니다.";
+    public static final String NOT_ROOM_USER = "대화방 사용자가 아닙니다.";
     public static final String TOKEN_EXPIRED = "만료된 토큰입니다.";
     public static final String UNABLE_JWT_SIGNATURE = "잘못된 JWT 서명입니다.";
     public static final String UNSUPPORTED_JWT_TOKEN = "지원하지 않는 JWT 토큰입니다.";
