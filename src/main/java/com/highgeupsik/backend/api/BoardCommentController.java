4f9239926e91206bc18d17de213fc95a33d99f60
baeseohyeon <formnarago@naver.com>
2022-06-04 16:22:13 +0900

    chore: 페이지 파라미터명, 디폴트 페이지사이즈 변경

--- src/main/java/com/highgeupsik/backend/api/BoardCommentController.java
+++ src/main/java/com/highgeupsik/backend/api/BoardCommentController.java
@@ -3,7 +3,6 @@ package com.highgeupsik.backend.api;
 import static com.highgeupsik.backend.utils.ApiUtils.*;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,9 +27,9 @@ public class BoardCommentController {
     public ApiResult<Page<CommentResDTO>> boardComments(
         @LoginUser Long userId,
         @PathVariable Long boardId,
-        @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
-        @RequestParam(value = "pageSize", defaultValue = "10") int pageSize
+        @RequestParam(value = "page", defaultValue = "1") int pageNum,
+        @RequestParam(value = "pageSize", defaultValue = "20") int pageSize
     ) {
-        return success(boardCommentService.findCommentsBy(userId, boardId, PageRequest.of(pageNum, pageSize)));
+        return success(boardCommentService.findCommentsBy(userId, boardId, pageNum, pageSize));
     }
 }
