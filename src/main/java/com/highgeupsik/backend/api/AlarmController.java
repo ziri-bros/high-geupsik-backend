f5fbaef9e6e27f9e0fcedbc7b38a4bb4199259c6
baeseohyeon <formnarago@naver.com>
2022-06-08 13:42:41 +0900

    feat: sse연결 api 추가

---
+++ src/main/java/com/highgeupsik/backend/api/AlarmController.java
@@ -0,0 +1,20 @@
+package com.highgeupsik.backend.api;
+
+import com.highgeupsik.backend.resolver.LoginUser;
+import com.highgeupsik.backend.service.AlarmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RequiredArgsConstructor
+@ResponseBody
+public class AlarmController {
+
+    private final AlarmService alarmService;
+
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    public SseEmitter subscribe(@LoginUser Long userId) {
+        return alarmService.subscribe(userId);
+    }
+}
