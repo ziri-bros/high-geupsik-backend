29b766f2deacf81893f76805188fedf165a3ed7c
baeseohyeon <formnarago@naver.com>
2022-06-08 13:49:42 +0900

    feat: Notification 메세지 추가

---
+++ src/main/java/com/highgeupsik/backend/utils/NotificationMessage.java
@@ -0,0 +1,8 @@
+package com.highgeupsik.backend.utils;
+
+public class NotificationMessage {
+
+    public static final String COMMENT_NOTIFICATION = "게시글에 댓글이 작성되었습니다.";
+    public static final String REPLY_NOTIFICATION = "답글이 작성되었습니다.";
+    public static final String MESSAGE_NOTIFICATION = "새로운 쪽지가 왔습니다.";
+}
