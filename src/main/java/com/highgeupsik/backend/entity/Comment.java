27554ff1184598b4650c605253c9878dff4397cd
baeseohyeon <formnarago@naver.com>
2022-06-12 14:51:01 +0900

    refactor: 댓글 자식리스트에 케스케이드 추가

--- src/main/java/com/highgeupsik/backend/entity/Comment.java
+++ src/main/java/com/highgeupsik/backend/entity/Comment.java
@@ -60,7 +60,7 @@ public class Comment extends TimeEntity {
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
-    @OneToMany(mappedBy = "parent")
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
     private List<Comment> children = new ArrayList<>();
 
     @Builder
