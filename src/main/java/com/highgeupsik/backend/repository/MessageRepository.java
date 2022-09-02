da24f272c7e430852771e67d5c419fc0ff91823b
baeseohyeon <formnarago@naver.com>
2022-09-02 16:07:30 +0900

    feat: MessageRepository readFlag 벌크쿼리 추가

--- src/main/java/com/highgeupsik/backend/repository/MessageRepository.java
+++ src/main/java/com/highgeupsik/backend/repository/MessageRepository.java
@@ -1,11 +1,19 @@
 package com.highgeupsik.backend.repository;
 
 import com.highgeupsik.backend.entity.Message;
+import com.highgeupsik.backend.entity.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     Page<Message> findAllByRoomIdAndOwnerId(Long roomId, Long ownerId, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = "update Message m set m.isRead = true where m.room = :room")
+    void updateReadFlagByRoom(@Param("room") Room room);
 }
