dbe07ac394335d548aa500574731378eaf8d602c
baeseohyeon <formnarago@naver.com>
2022-09-03 19:52:12 +0900

    chore: error message 영어로 수정

--- src/main/java/com/highgeupsik/backend/utils/ErrorMessage.java
+++ src/main/java/com/highgeupsik/backend/utils/ErrorMessage.java
@@ -2,22 +2,22 @@ package com.highgeupsik.backend.utils;
 
 public class ErrorMessage {
 
-    public static final String USER_NOT_FOUND = "없는 사용자 입니다.";
-    public static final String BOARD_NOT_FOUND = "없는 게시글 입니다.";
-    public static final String COMMENT_NOT_FOUND = "없는 댓글 입니다.";
-    public static final String SCHOOL_NOT_FOUND = "없는 학교입니다.";
-    public static final String GRADE_NOT_FOUND = "잘못된 학년 입니다.";
-    public static final String ROOM_NOT_FOUND = "없는 대화방 입니다.";
-    public static final String NOTIFICATION_NOT_FOUND = "없는 알림 입니다.";
-    public static final String WRITER_NOT_MATCH = "작성자가 아닙니다.";
-    public static final String NOT_USER = "사용자가 아닙니다.";
-    public static final String NOT_ROLE_USER = "권한이 없는 사용자 입니다.";
-    public static final String NOT_ROOM_USER = "대화방 사용자가 아닙니다.";
-    public static final String TOKEN_EXPIRED = "만료된 토큰입니다.";
-    public static final String UNABLE_JWT_SIGNATURE = "잘못된 JWT 서명입니다.";
-    public static final String UNSUPPORTED_JWT_TOKEN = "지원하지 않는 JWT 토큰입니다.";
-    public static final String WRONG_JWT_TOKEN = "잘못된 JWT 토큰입니다.";
-    public static final String REGION_NOT_NULL = "지역 입력은 필수입니다.";
-    public static final String CATEGORY_NOT_NULL = "카테고리 입력은 필수입니다.";
-    public static final String SSE_CONNECT_ERROR = "SSE 연결에 실패했습니다.";
+    public static final String USER_NOT_FOUND = "user not found";
+    public static final String BOARD_NOT_FOUND = "board not found";
+    public static final String COMMENT_NOT_FOUND = "comment not found";
+    public static final String SCHOOL_NOT_FOUND = "school not found";
+    public static final String GRADE_NOT_FOUND = "grade not found";
+    public static final String ROOM_NOT_FOUND = "room not found";
+    public static final String NOTIFICATION_NOT_FOUND = "notification not found";
+    public static final String WRITER_NOT_MATCH = "writer not match";
+    public static final String NOT_USER = "not user";
+    public static final String NOT_ROLE_USER = "not user role";
+    public static final String NOT_ROOM_USER = "not room user";
+    public static final String TOKEN_EXPIRED = "expired token";
+    public static final String UNABLE_JWT_SIGNATURE = "unable jwt signature";
+    public static final String UNSUPPORTED_JWT_TOKEN = "unsupported jwt token";
+    public static final String WRONG_JWT_TOKEN = "wrong jwt token";
+    public static final String REGION_NOT_NULL = "region not null";
+    public static final String CATEGORY_NOT_NULL = "category not null";
+    public static final String SSE_CONNECT_ERROR = "sse connection error";
 }
