e59e56b429f26f7dc0c62d341e0f2749c029ba75
baeseohyeon <formnarago@naver.com>
2022-10-16 23:13:12 +0900

    chore: 커스텀 시간표 클래스 삭제

--- src/main/java/com/highgeupsik/backend/repository/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.highgeupsik.backend.repository;
-
-import com.highgeupsik.backend.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-
-    void deleteBySubjectScheduleId(Long subjectScheduleId);
-}
