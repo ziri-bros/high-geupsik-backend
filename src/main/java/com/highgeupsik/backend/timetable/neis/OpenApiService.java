4cf16fe9a629a856102beeb10138552c12ef4f1d
baeseohyeon <formnarago@naver.com>
2022-10-16 23:56:57 +0900

    feat: Open API 요청 서비스 추가

---
+++ src/main/java/com/highgeupsik/backend/timetable/neis/OpenApiService.java
@@ -0,0 +1,38 @@
+package com.highgeupsik.backend.timetable.neis;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OpenApiService {
+
+    private ObjectMapper mapper = new ObjectMapper().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+
+    public <T> T getRequest(String url, Class<T> clazz) {
+        BufferedReader br = null;
+        try {
+            URL u = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) u.openConnection();
+            conn.setRequestMethod("GET");
+            br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            return mapper.readValue(br.readLine(), clazz);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+}
